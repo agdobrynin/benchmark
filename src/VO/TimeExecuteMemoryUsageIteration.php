2261b472f76ab95507a2ffb8741a2a51018aa3f4
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-08-23 11:22:08 +0400

    Fixing description for parameters `TimeExecuteMemoryUsageIteration`.

--- src/VO/TimeExecuteMemoryUsageIteration.php
+++ src/VO/TimeExecuteMemoryUsageIteration.php
@@ -6,6 +6,15 @@ namespace Kaspi\Benchmark\VO;
 
 final class TimeExecuteMemoryUsageIteration
 {
+    /**
+     * @param int   $startMemoryUsage     memory usage value in bytes before an execution iteration
+     * @param int   $endMemoryUsage       memory usage value in bytes after an execution iteration
+     * @param int   $startMemoryPeakUsage peak memory usage value in bytes before an execution iteration
+     * @param int   $endMemoryPeakUsage   peak memory usage value in bytes after an execution iteration
+     * @param float $startHrTime          system time in nanoseconds before an execution iteration
+     * @param float $endHrTime            system time in nanoseconds after an execution iteration
+     * @param int   $numberOfTimes        number of benchmark runs within a single iteration
+     */
     public function __construct(
         public readonly int $startMemoryUsage,
         public readonly int $endMemoryUsage,
