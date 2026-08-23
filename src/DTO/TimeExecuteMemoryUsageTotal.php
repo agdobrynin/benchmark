c22d04b61613eff865437481cf8d52b5468e9f9f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-08-23 11:12:53 +0400

    Fixing description for parameters `TimeExecuteMemoryUsageTotal`.

--- src/DTO/TimeExecuteMemoryUsageTotal.php
+++ src/DTO/TimeExecuteMemoryUsageTotal.php
@@ -6,6 +6,13 @@ namespace Kaspi\Benchmark\DTO;
 
 final class TimeExecuteMemoryUsageTotal
 {
+    /**
+     * @param float $memoryUsage     memory usage in bytes
+     * @param float $memoryPeakUsage memory peak usage in bytes
+     * @param float $hrTime          execute time in nanoseconds
+     * @param int   $iterations      number of benchmark runs
+     * @param int   $numberOfTimes   number of benchmark runs within a single iteration
+     */
     public function __construct(
         public readonly float $memoryUsage,
         public readonly float $memoryPeakUsage,
