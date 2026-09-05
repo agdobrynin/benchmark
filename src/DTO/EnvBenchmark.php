3a6b36f8cff086526fe9b82720d78761b3c50ef2
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-09-05 21:51:58 +0400

    The `EnvBenchmark` class now implements the `\Stringable` interface.

--- src/DTO/EnvBenchmark.php
+++ src/DTO/EnvBenchmark.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace Kaspi\Benchmark\DTO;
 
-use function implode;
-use function md5;
+use Stringable;
 
-final class EnvBenchmark
+use function implode;
+use function intdiv;
+use function md5;
+use function sprintf;
+
+final class EnvBenchmark implements Stringable
 {
     public function __construct(public readonly int $phpVersionId, public readonly bool $opcacheEnableCli) {}
+
+    public function __toString(): string
+    {
+        $opCache = $this->opcacheEnableCli ? 'on' : 'off';
+        $major = intdiv($this->phpVersionId, 10_000);
+        $minor = intdiv($this->phpVersionId % 10_000, 100);
+        $release = $this->phpVersionId % 100;
+
+        return sprintf('PHP runtime: %d.%d.%d , OPCache: %s', $major, $minor, $release, $opCache);
+    }
 
     public function toHash(): string
     {
